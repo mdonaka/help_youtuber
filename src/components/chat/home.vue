d9095d8e2d58af0785db8fbfb88316be01c11ebe
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2020-02-05 18:45:02 +0900

    add chat system

--- src/components/chat/home.vue
+++ src/components/chat/home.vue
@@ -1,13 +1,51 @@
 <template>
-  <div id="app">
-		chat画面
+  <div>
+		<h1>chat画面</h1>
+		<div v-for="text in textList" v-bind:key="text.id">
+			{{text}}
+		</div>
+		<input v-model="sendText">
+		<button @click="sendMessage">send</button>
   </div>
 </template>
 
 <script>
+/* eslint-disable no-console */
+
+const initialData = ()=>{
+	return {
+		sendText: "",
+		textList: [],
+		sock: new WebSocket(process.env.VUE_APP_CHAT_URL)
+	};
+}
+
 
 export default {
+	data(){
+		return initialData()
+	},
+	created(){
+		// 接続
+		this.sock.addEventListener('open',function(){
+			console.log('Socket 接続成功');
+		});
+
+		const data = this;
+		// サーバーからデータを受け取る
+		this.sock.addEventListener('message',function(e){
+			data.textList.push(e.data);
+		});
+	},
+	methods:{
+		sendMessage: function(){
+			const data = JSON.stringify({"action":"sendMessage", "data":this.sendText});
+			this.sock.send(data);
+			this.sendText= "";
+		}
+	}
 }
+
 </script>
 
 <style>
