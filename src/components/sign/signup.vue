bfb2b2b8b9f2387a5f9b1e23dc96abe67940e677
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2020-02-10 08:58:41 +0900

    add signup system

--- src/components/sign/signup.vue
+++ src/components/sign/signup.vue
@@ -1,13 +1,60 @@
 <template>
 	<div>
 		<h2>sign up</h2>
+		<!-- 仮置き -->
+		<form>
+			mail:<input type="text" v-model="mail">
+			pass:<input type="text" v-model="password">
+			nick:<input type="text" v-model="nickname">
+			<input type="submit" @click="signup">
+		</form>
+		{{mail}},{{password}},{{nickname}}
+
+		<!-- ------ -->
 		<div>no bug</div>
 	</div>
 </template>
 
 <script>
 /* eslint-disable no-console */
+// const aws = require("aws-sdk");
+const cognito = require("amazon-cognito-identity-js");
+const poolData = {
+	UserPoolId: process.env.VUE_APP_USER_POOL_ID,
+	ClientId: process.env.VUE_APP_CLIENT_ID,
+};
+const userPool = new cognito.CognitoUserPool(poolData);
+
+const initialData = ()=>{
+	return {
+		mail:"test@tsetaa.ppas.aaa",
+		password:"1234**KKoop",
+		nickname:"nickname",
+	};
+}
+
 export default {
+	data(){
+		return initialData()
+	},
+	created(){
+	},
+	methods:{
+		signup:function(){
+			let attributeList = [];
+			const dataNickName = {
+				Name: "nickname",
+				value: this.nickname
+			}
+			attributeList.push(new cognito.CognitoUserAttribute(dataNickName));
+
+			userPool.signUp(this.mail, this.password, attributeList, null, (err, result)=>{
+				if(err){console.log(err);return;}
+				console.log("success");
+				console.log(result);
+			});
+		}
+	}
 }
 </script>
 
