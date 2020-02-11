54778d401ff797a6a72567effe5609e85e5c355c
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2020-02-11 15:49:43 +0900

    fix signin router

--- src/components/sign/home.vue
+++ src/components/sign/home.vue
@@ -3,7 +3,7 @@
 		<h2>sign up, sign in</h2>
 		<router-view/>
 		<ul>
-			<li><router-link to="signin/">Signin</router-link></li>
+			<li><router-link to="sign/signin/">Signin</router-link></li>
 			<li><router-link to="sign/signup">Sign upはこちらから</router-link></li>
 		</ul>
 	</div>
