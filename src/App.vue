482a98f325fcf6479758f5c05b8f636e68918dda
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2020-02-19 19:50:13 +0900

    add use id hold

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,15 @@
 </template>
 
 <script>
+/* eslint-disable no-console */
 
 import {mapState, mapMutations} from 'vuex'
+const cognito = require("amazon-cognito-identity-js");
+const poolData = {
+	UserPoolId: process.env.VUE_APP_USER_POOL_ID,
+	ClientId: process.env.VUE_APP_CLIENT_ID,
+};
+const userPool = new cognito.CognitoUserPool(poolData);
 
 export default {
   name: 'app',
@@ -23,8 +30,38 @@ export default {
 			this.$router.push("/sign");
 		},
 		...mapMutations({
-			logout:"id/logout"
+			logout:"id/logout",
+			login:"id/login"
 		}),
+	},
+	created(){
+	
+		const cognitoUser = userPool.getCurrentUser();  // 現在のユーザー
+		let currentUserData = {};  // ユーザーの属性情報
+		const login = this.login;
+		// 現在のユーザー情報が取得できているか？
+		if (cognitoUser != null) {
+			cognitoUser.getSession(function(err) {
+				if (err) {
+					console.log(err);
+				} else {
+					// ユーザの属性を取得
+					cognitoUser.getUserAttributes(function(err, result) {
+						if (err) {
+							console.log(err);
+						}
+						// 取得した属性情報を連想配列に格納
+						for (let i = 0; i < result.length; i++) {
+							currentUserData[result[i].getName()] = result[i].getValue();
+						}
+						login(currentUserData["sub"]);
+						console.log(currentUserData);
+					});
+				}
+			});
+		}else{
+			console.log("non");
+		} 	
 	}
 }
 </script>
--- src/components/sign/signin.vue
+++ src/components/sign/signin.vue
@@ -8,6 +8,7 @@
 			pass:<input type="text" v-model="password">
 			<input type="submit" @click="signin">
 		</form>
+		<button @click="test">get</button>
 		{{mail}},{{password}}
 	</div>
 </template>
@@ -26,7 +27,7 @@ const userPool = new cognito.CognitoUserPool(poolData);
 
 const initialData = ()=>{
 	return {
-		mail: "",
+		mail: "nickname",
 		password: "1234**KKoop"
 	};
 };
@@ -52,22 +53,49 @@ export default {
 				Pool: userPool
 			};
 			const cognitoUser = new cognito.CognitoUser(userData);
-			const login = this.login;
 			// 認証処理
 			cognitoUser.authenticateUser(authenticationDetails, {
-				onSuccess: function (result) {
-					var idToken = result.getIdToken().getJwtToken();          // IDトークン
+				onSuccess: function () {
+					//var idToken = result.getIdToken().getJwtToken();          // IDトークン
 					//var accessToken = result.getAccessToken().getJwtToken();  // アクセストークン
 					//var refreshToken = result.getRefreshToken().getToken();   // 更新トークン
-					login(idToken);
-					console.log(idToken);
-					// サインイン成功の場合、次の画面へ遷移
+					console.log("Success");
 				},
 				onFailure: function(err) {
 					// サインイン失敗の場合、エラーメッセージを画面に表示
 					console.log(err);
 				}
 			});
+			this.test();
+		},
+		test: function(){
+			const cognitoUser = userPool.getCurrentUser();  // 現在のユーザー
+			let currentUserData = {};  // ユーザーの属性情報
+			const login = this.login;
+			// 現在のユーザー情報が取得できているか？
+			if (cognitoUser != null) {
+				cognitoUser.getSession(function(err) {
+					if (err) {
+						console.log(err);
+					} else {
+						// ユーザの属性を取得
+						cognitoUser.getUserAttributes(function(err, result) {
+							if (err) {
+								console.log(err);
+							}
+
+							// 取得した属性情報を連想配列に格納
+							for (let i = 0; i < result.length; i++) {
+								currentUserData[result[i].getName()] = result[i].getValue();
+							}
+							login(currentUserData["sub"]);
+							console.log(currentUserData);
+						});
+					}
+				});
+			}else{
+				console.log("non");
+			} 	
 		}
 	}
 }
