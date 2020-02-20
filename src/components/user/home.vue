1154ef1c2057630dc91facf38fed38389979a1bc
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2020-02-20 18:25:27 +0900

    add user page

---
+++ src/components/user/home.vue
@@ -0,0 +1,44 @@
+<template>
+	<div> 
+		<h2>ユーザ画面</h2>
+		<h1 @click="post">POST チェック</h1>
+	</div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+
+export default {
+	created:()=>{
+		/*
+		console.log("Hello");
+		$(function() {
+			$('#ajax-button').click(
+				function() {
+					var hostUrl= 'http://localhost:8000';
+					var param1 = 1;
+					var param2 = 10;
+					$.ajax({
+					url: hostUrl,
+					type:'POST',
+					dataType: 'json',
+					data : {parameter1 : param1, parameter2 : param2 },
+					timeout:3000,
+				}).done(function(data) {
+					alert("ok");
+				}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+					alert("error");
+				})
+			});
+		});
+		*/
+	},
+	methods:{
+		post: ()=>{
+		},
+	}
+}
+</script>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Chat from '@/components/chat/home'
+import User from '@/components/user/home'
 import Sign from '@/components/sign/home'
 import Signin from '@/components/sign/signin'
 import Signup from '@/components/sign/signup'
@@ -15,6 +16,7 @@ export default new Router({
 			component: Home, 
 			children: [
 				{ path: '/chat', component: Chat },
+				{ path: '/user', component: User },
 			]
 		},
 		{
