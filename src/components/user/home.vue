e3d00c487348722f2e73395a83c4643802bda01b
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2020-04-29 15:17:22 +0900

    add get from DB

--- src/components/user/home.vue
+++ src/components/user/home.vue
@@ -1,7 +1,8 @@
 <template>
 	<div> 
 		<h2>ユーザ画面</h2>
-		<h1 @click="post">POST チェック</h1>
+		<h1 @click="update">POST チェック</h1>
+		<h1 @click="get">GET チェック</h1>
 	</div>
 </template>
 
@@ -14,17 +15,35 @@ export default {
 	created:()=>{
 	},
 	methods:{
-		post: ()=>{
+		get:()=>{
 			const axios_obj = Axios.create({
 				responseType: 'json'
 			});
-
-			const data = JSON.stringify({str:"Hello"});
-			const API = process.env.VUE_APP_DB_API + "updateDB";
-
+			const API = process.env.VUE_APP_DB_API + "getDB";
+			axios_obj.get(API, {patams:{id:"omiomi"}}).then(response => {
+				const data = response.data;
+				console.log("data:");
+				console.log(data);
+			});
+		},
+		post:(method, data, callback)=>{
+			const axios_obj = Axios.create({
+				responseType: 'json'
+			});
+			const API = process.env.VUE_APP_DB_API + method;
 			axios_obj.post(API, data).then(response => {
 				const data = response.data;
-				console.log(data);
+				callback(data);
+			});
+		},
+		update:function(){
+			const data = JSON.stringify({
+				id:"omitsu",
+				Twitter: "@omiomi",
+				name: "SAN"
+			});
+			this.post("updateDB", data, (response)=>{
+				console.log(response);
 			});
 		},
 	}
