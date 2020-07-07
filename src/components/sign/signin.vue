9e4579654987ba36c2f189fb67dc24ac6248ca17
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2020-07-07 16:39:48 +0900

    add signin loading

--- src/components/sign/signin.vue
+++ src/components/sign/signin.vue
@@ -1,6 +1,21 @@
 <template>
   <v-app>
-    <v-card width="400px" class="mx-auto mt-5">
+    <v-card width="400px" class="mx-auto mt-5" v-bind:disabled="nowLoading" >
+			<!-- ログイン時のローディング処理 -->
+			<v-dialog width="400px" v-model="nowLoading" persistent hide-overlay>
+				<v-card color="primary" dark >
+					<v-card-text>
+						now login...
+						<v-progress-linear
+							indeterminate
+							color="white"
+							class="mb-0"
+						></v-progress-linear>
+					</v-card-text>
+				</v-card>
+			</v-dialog>
+			<!-- ---------------------------- -->
+
       <v-card-title>
         <h1 class="display-1">ログイン</h1>
       </v-card-title>
@@ -27,7 +42,8 @@ const initialData = ()=>{
 	return {
 		mail: "nickname",
 		password: "1234**KKoop",
-		showPassword:false
+		showPassword:false,
+		nowLoading:false
 	};
 };
 
@@ -41,14 +57,17 @@ export default {
 			login: "id/login_new",
 		}),
 		signin:function(){
+			this.nowLoading = true;
 			// 認証データの作成
 			const authenticationData = {
 				Username: this.mail,
 				Password: this.password
 			};
-			const ok =  this.login(authenticationData);
-			ok.then((v)=>{
-				console.log(v);
+			const thisR = this;
+			this.login(authenticationData).then(()=>{
+				thisR.nowLoading = false;
+			},(err)=>{
+				console.log(err);
 			});
 		},
 	}
