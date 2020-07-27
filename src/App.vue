a53c214a6ff26b7530532e3e164a5db097af1b3a
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2020-07-27 19:28:03 +0900

    update home

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,5 @@
 <template>
   <v-app>
-    <v-toolbar color="error" dense>
-          <v-img
-          alt="YtoE Logo"
-          class="shrink mr-2"
-          contain
-          v-bind:src="require('@/assets/YtoE_logo.png')"
-          width="60"
-        />
-        <v-col cols="12" sm="5">
-        <v-text-field
-          hide-details
-          label="検索"
-          solo
-          dense
-        ></v-text-field>
-        </v-col>
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-        <v-spacer></v-spacer>
-  
-        <v-btn icon href="#/chat" >
-          <v-icon>mdi-chat</v-icon>
-        </v-btn>
-  
-        <v-btn icon href="#/user">
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
-        <v-btn icon href="/#/">
-          <v-icon>mdi-home</v-icon>
-        </v-btn>
-      </v-toolbar>
-
-		<!-- debug用 -->
-		<v-btn icon @click=logout>
-			<v-icon>mdi-wifi-off</v-icon>
-		</v-btn>
-		(debug用)ユーザID:{{id}}
-		<!-- ------- -->
     <v-content>
       <router-view/>
     </v-content>
@@ -47,26 +8,8 @@
 
 <script>
 /* eslint-disable no-console */
-import {mapState, mapActions} from 'vuex'
 
 export default {
 	name: 'app',
-	computed: {
-		...mapState({
-			id:s=>s.id.id
-		}),
-	},
-	methods:{
-		hello: function(){
-			console.log("on");
-		},
-		...mapActions({
-			logout:"id/logout",
-			login:"id/login"
-		}),
-	},
-	created(){
-		this.login().then((res) => {console.log(res);});
-	}
 };
 </script>
