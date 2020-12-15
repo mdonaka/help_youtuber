15cf87573b44ce7747c5dbd172db2e1cfb0391b5
masa0410 <m.r.o.a1224@ezweb.ne.jp>
2020-12-15 19:43:17 +0900

    dir change

--- src/router.js
+++ src/router.js
@@ -5,15 +5,15 @@ import Branch from '@/components/both/id_check'
 import Sign from '@/components/both/sign/home'
 import Signin from '@/components/both/sign/signin'
 import Signup from '@/components/both/sign/signup'
-/*
+
 import Chat from '@/components/editor/chat/home'
 import UserPage from '@/components/editor/user/home'
 import Home from '@/components/editor/Home/home'
-*/
+/*
 import Chat from '@/components/youtuber/chat/home'
 import UserPage from '@/components/youtuber/user/home'
 import Home from '@/components/youtuber/Home/home'
- 
+ */
 Vue.use(Router)
  
 export default new Router({
